8c2b07ac1833e36c89fa4629874d2164459dd242
JeonSangHyuck <101720393+JeonSangHyuck@users.noreply.github.com>
2022-05-27 18:27:30 +0900

    Update item.java

--- docs/res/Java/item.java
+++ docs/res/Java/item.java
@@ -1,1 +1,13 @@
 
+package com.cookandroid.test0427;
+
+public class item {
+    private String data;
+
+    public item(String _data){
+        data = _data;
+    }
+    public String getData() {
+        return data;
+    }
+}
